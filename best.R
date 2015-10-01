6b0a546e8f84aa8d9e9d3f4fb38f82cbfa7823d3
gedae <gedae@bluekitten.(none)>
2015-10-01 11:51:46 -0400

    read as characters

--- best.R
+++ best.R
@@ -1,9 +1,9 @@
 best <- function(state,outcome) {
-  x <- read.csv("outcome-of-care-measures.csv")
+  x <- read.csv("outcome-of-care-measures.csv",colClasses="character",na.strings="Not Available")
   st <- x[x$State == state,]
   if (nrow(st) == 0) stop("invalid state")
   header <- make.names(paste("Hospital.30.Day.Death..Mortality..Rates.from", gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", outcome, perl=TRUE)))
   if (!(header %in% colnames(x))) stop("invalid outcome")
   col <- st[,header]
-  as.character(st[which.min(as.numeric(as.character(col))),2])
+  st[which.min(as.numeric(col)),2]
 }
