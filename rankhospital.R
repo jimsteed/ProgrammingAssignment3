125e0c8c143ad40f15008e298d1e170939736623
gedae <gedae@bluekitten.(none)>
2015-10-01 12:29:34 -0400

    add sort by name as 2ndary criteria

--- rankhospital.R
+++ rankhospital.R
@@ -5,7 +5,7 @@ rankhospital <- function(state,outcome,num="best") {
   header <- make.names(paste("Hospital.30.Day.Death..Mortality..Rates.from", gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", outcome, perl=TRUE)))
   if (!(header %in% colnames(x))) stop("invalid outcome")
   col <- as.numeric(st[,header])
-  o <- order(col,na.last=NA)
+  o <- order(col,st$Hospital.Name,na.last=NA)
   if (num == "best") num <- 1
   if (num == "worst") num <- length(o)
   st[o[num],2]
